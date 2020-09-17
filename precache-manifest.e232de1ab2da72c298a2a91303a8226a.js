2e2f8633b99a6b4d6c339d7690c4fc846b8ea7e4
陳鎮宇 <ccking37@gmail.com>
2020-09-17 22:36:45 +0800

    Updates

---
+++ precache-manifest.e232de1ab2da72c298a2a91303a8226a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8694c57bc5a83ecdc969425828b7056",
+    "url": "/infinity-scroll-react/index.html"
+  },
+  {
+    "revision": "ab83c9fc0e09f1d9351f",
+    "url": "/infinity-scroll-react/static/css/main.1dacef1a.chunk.css"
+  },
+  {
+    "revision": "6584e3ae82449b04d86f",
+    "url": "/infinity-scroll-react/static/js/2.54be7aeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infinity-scroll-react/static/js/2.54be7aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab83c9fc0e09f1d9351f",
+    "url": "/infinity-scroll-react/static/js/main.87f7f28f.chunk.js"
+  },
+  {
+    "revision": "a3ec919549df58740a3c",
+    "url": "/infinity-scroll-react/static/js/runtime-main.48dd71ee.js"
+  },
+  {
+    "revision": "41c85791371fed8c4eb2a2a0cac157c2",
+    "url": "/infinity-scroll-react/static/media/loader.41c85791.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/infinity-scroll-react/precache-manifest.e232de1ab2da72c298a2a91303a8226a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/infinity-scroll-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
